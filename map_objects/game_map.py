80c910b1508c435c8b951ad66c7b6c71e9626e28
SalCilia <scilia@gmail.com>
2020-05-22 18:07:05 -0700

    Half way through tutorial 3.It works

--- map_objects/game_map.py
+++ map_objects/game_map.py
@@ -1,3 +1,4 @@
+from map_objects.rectangle import Rect
 from map_objects.tile import Tile
 
 
@@ -8,16 +9,26 @@ class GameMap:
         self.tiles = self.initialize_tiles()
 
     def initialize_tiles(self):
-        tiles = [[Tile(False) for y in range(self.height)] for x in range(self.width)]
-    
-        tiles[30][22].blocked = True
-        tiles[30][22].block_sight = True
-        tiles[31][22].blocked = True
-        tiles[31][22].block_sight = True
-        tiles[32][22].blocked = True
-        tiles[32][22].block_sight = True
-
+        tiles = [[Tile(True) for y in range(self.height)] for x in range(self.width)]
         return tiles
+    
+    def make_map(self):
+        # Create two rooms for demonstration purposes
+        room1 = Rect(20, 15, 10, 15)
+        room2 = Rect(35, 15, 10, 15)
+
+        self.create_room(room1)
+        self.create_room(room2)
+
+    def create_room(self, room):
+        # go through the tiles in the rectangle and make them passable
+        for x in range(room.x1 + 1, room.x2):
+            for y in range(room.y1 + 1, room.y2):
+                self.tiles[x][y].blocked = False
+                self.tiles[x][y].block_sight = False
+
+
+        
 
     def is_blocked(self,x,y):
         if self.tiles[x][y].blocked:
